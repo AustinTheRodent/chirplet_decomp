02948c541cf033d20cf4312635edaf1b139c1973
AustinTheRodent <89956165+AustinTheRodent@users.noreply.github.com>
2023-11-28 23:16:46 -0600

    Delete ip_repo/chirplet_gen_v1 directory

--- ip_repo/chirplet_gen_v1/exponential_lut/gen_exponent.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-
-for i in range(2**16):
-  print(np.exp(-32.0*(float(i))/(2**16)))
-
-#print(np.exp(0))
--- ip_repo/chirplet_gen_v1/sine_lut/gen_sine.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-
-for i in range(2**16):
-  print(np.sin(2*np.pi*(float(i))/(2**16)))
-
-
-#print(np.exp(0))
